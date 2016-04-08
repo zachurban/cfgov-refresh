c7422d984363c154384119595e9f17ad0efb8b5e
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2016-04-08 11:50:59 -0400

    Scope CSS

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -6,6 +6,8 @@ var mqr = require( 'gulp-mq-remove' );
 var pkg = require( '../config' ).pkg;
 var banner = require( '../config' ).banner;
 var config = require( '../config' ).styles;
+var glob = require( 'glob' );
+var path = require( 'path' );
 var handleErrors = require( '../utils/handleErrors' );
 var browserSync = require( 'browser-sync' );
 
@@ -64,21 +66,31 @@ function stylesIe() {
  * @returns {PassThrough} A source stream.
  */
 function stylesOnDemand() {
-  return gulp.src( config.cwd + '/on-demand/*.less' )
-    .pipe( $.less( config.settings ) )
-    .on( 'error', handleErrors )
-    .pipe( $.autoprefixer( {
-      browsers: [ 'last 2 version',
-                  'ie 7-8',
-                  'android 4',
-                  'BlackBerry 7',
-                  'BlackBerry 10' ]
-    } ) )
-    .pipe( $.header( banner, { pkg: pkg } ) )
-    .pipe( gulp.dest( config.dest ) )
-    .pipe( browserSync.reload( {
-      stream: true
-    } ) );
+  var files = glob.sync( config.cwd + '/on-demand/**/*.less' );
+  var streams = files.map( function( file ) {
+    var dirs = path.dirname( file ).split('/');
+    var prefix = '.' + dirs[ dirs.length-1 ].charAt(0) + '-';
+    var className = prefix + path.basename( file, '.less' );
+    return gulp.src( file )
+      .pipe( $.less( config.settings ) )
+      .on( 'error', handleErrors )
+      .pipe( $.autoprefixer( {
+        browsers: [ 'last 2 version',
+                    'ie 7-8',
+                    'android 4',
+                    'BlackBerry 7',
+                    'BlackBerry 10' ]
+      } ) )
+      .pipe( $.header( banner, { pkg: pkg } ) )
+      .pipe( $.wrapper( {
+        header: className + ' {',
+        footer: '}'
+      } ) )
+      .pipe( gulp.dest( config.dest ) )
+      .pipe( browserSync.reload( {
+        stream: true
+      } ) );
+  } );
 }
 
 gulp.task( 'styles:modern', stylesModern );
