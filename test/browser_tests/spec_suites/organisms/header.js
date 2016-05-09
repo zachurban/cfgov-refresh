9c9a761fc8a264145adfcb201aa7d7300312f92a
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2016-05-09 17:58:19 -0400

    Adds additional header tests

--- test/browser_tests/spec_suites/organisms/header.js
+++ test/browser_tests/spec_suites/organisms/header.js
@@ -7,8 +7,13 @@ var breakpointsConfig = require( BASE_JS_PATH + 'config/breakpoints-config' );
 describe( 'Header', function() {
   var BASE_SEL = '.o-header';
   var LOGO_SEL = BASE_SEL + '_logo-img';
+  var OVERLAY_SEL = '.a-overlay';
   var MEGA_MENU_SEL = BASE_SEL + ' .o-mega-menu';
+  var MEGA_MENU_TRIGGER_SEL = BASE_SEL + ' .o-mega-menu_trigger';
+  var MEGA_MENU_CONTENT_SEL = BASE_SEL + ' .o-mega-menu_content';
   var GLOBAL_SEARCH_SEL = BASE_SEL + ' .m-global-search';
+  var GLOBAL_SEARCH_TRIGGER_SEL = BASE_SEL + ' .m-global-search_trigger';
+  var GLOBAL_SEARCH_CONTENT_SEL = BASE_SEL + ' .m-global-search_content';
   var GLOBAL_CTA_LG_SEL = BASE_SEL + ' .m-global-header-cta__horizontal';
   var GLOBAL_CTA_SM_SEL = BASE_SEL + ' .m-global-header-cta__list';
   var GLOBAL_EYEBROW_LG_SEL = BASE_SEL + ' .m-global-eyebrow__horizontal';
@@ -18,14 +23,19 @@ describe( 'Header', function() {
 
   beforeAll( function() {
     _dom = {
-      header:            element( by.css( BASE_SEL ) ),
-      logo:              element( by.css( LOGO_SEL ) ),
-      megaMenu:          element( by.css( MEGA_MENU_SEL ) ),
-      globalSearch:      element( by.css( GLOBAL_SEARCH_SEL ) ),
-      globalHeaderCtaLG: element( by.css( GLOBAL_CTA_LG_SEL ) ),
-      globalHeaderCtaSM: element( by.css( GLOBAL_CTA_SM_SEL ) ),
-      globalEyebrowLG:   element( by.css( GLOBAL_EYEBROW_LG_SEL ) ),
-      globalEyebrowSM:   element( by.css( GLOBAL_EYEBROW_SM_SEL ) )
+      header:              element( by.css( BASE_SEL ) ),
+      logo:                element( by.css( LOGO_SEL ) ),
+      overlay:             element( by.css( OVERLAY_SEL ) ),
+      megaMenu:            element( by.css( MEGA_MENU_SEL ) ),
+      megaMenuTrigger:     element( by.css( MEGA_MENU_TRIGGER_SEL ) ),
+      megaMenuContent:     element.all( by.css( MEGA_MENU_CONTENT_SEL ) ).first(),
+      globalSearch:        element( by.css( GLOBAL_SEARCH_SEL ) ),
+      globalSearchTrigger: element( by.css( GLOBAL_SEARCH_TRIGGER_SEL ) ),
+      globalSearchContent: element( by.css( GLOBAL_SEARCH_CONTENT_SEL ) ),
+      globalHeaderCtaLG:   element( by.css( GLOBAL_CTA_LG_SEL ) ),
+      globalHeaderCtaSM:   element( by.css( GLOBAL_CTA_SM_SEL ) ),
+      globalEyebrowLG:     element( by.css( GLOBAL_EYEBROW_LG_SEL ) ),
+      globalEyebrowSM:     element( by.css( GLOBAL_EYEBROW_SM_SEL ) )
     };
   } );
 
@@ -85,6 +95,10 @@ describe( 'Header', function() {
           expect( _dom.megaMenu.isDisplayed() ).toBe( true );
         } );
 
+        it( 'should NOT display the global overlay', function() {
+          expect( _dom.overlay.isDisplayed() ).toBe( false );
+        } );
+
         it( 'should display Global Search', function() {
           expect( _dom.globalSearch.isDisplayed() ).toBe( true );
         } );
@@ -93,26 +107,60 @@ describe( 'Header', function() {
           expect( _dom.globalHeaderCtaLG.isDisplayed() ).toBe( false );
         } );
 
-        xit( 'should display small Global Header CTA', function() {
-          // TODO: Click mega menu to show Global Header CTA.
-          //       Or move this to the mega menu test.
-          expect( _dom.globalHeaderCtaSM.isDisplayed() ).toBe( true );
+        it( 'should display small Global Header CTA', function() {
+          browser.driver.actions().click( _dom.megaMenuTrigger ).perform()
+            .then( function(){
+              expect( _dom.globalHeaderCtaSM.isDisplayed() ).toBe( true );
+            } );
         } );
 
         it( 'should NOT display large Global Eyebrow', function() {
           expect( _dom.globalEyebrowLG.isDisplayed() ).toBe( false );
         } );
 
-        xit( 'should display small Global Eyebrow', function() {
-          // TODO: Click mega menu to show Global Eyebrow.
-          //       Or move this to the mega menu test.
-          expect( _dom.globalEyebrowSM.isDisplayed() ).toBe( true );
+        it( 'should display small Global Eyebrow', function() {
+          browser.driver.actions().click( _dom.megaMenuTrigger ).perform()
+            .then( function(){
+              expect( _dom.globalEyebrowSM.isDisplayed() ).toBe( true );
+            } );
         } );
       } );
 
-      // TODO: Add tests for clicking between menu and search.
-      // describe( 'click search when mega menu is showing', function() {} );
-      // describe( 'click mega menu when search is showing', function() {} );
+      describe( 'click mega menu', function() {
+        it( 'should show the global overlay', function() {
+          browser.driver.actions().click( _dom.megaMenuTrigger ).perform()
+            .then( function(){
+              expect( _dom.overlay.isDisplayed() ).toBe( true );
+            } );
+        } );
+      } );
+
+      describe( 'click mega menu and click search', function() {
+        it( 'should hide the mega menu and show search', function() {
+          browser.driver.actions().click( _dom.megaMenuTrigger ).perform()
+            .then( function(){
+              browser.driver.actions().click( _dom.globalSearchTrigger )
+                .perform().then( function() {
+                  browser.driver.findElement( by.css( MEGA_MENU_CONTENT_SEL ) )
+                    .then( function( elem ) {
+                      browser.wait(
+                        protractor.until.elementIsNotVisible( elem )
+                      ).then( function() {
+                        expect( _dom.megaMenuTrigger.isDisplayed() ).toBe( false );
+                        expect( _dom.globalSearchTrigger.isDisplayed() ).toBe( true );
+                      } );
+                    } );
+                } );
+            } );
+        } );
+      } );
+
+      // describe( 'click search and click mega menu', function() {} );
+      describe( 'click search and click mega menu', function() {
+        xit( 'should hide the search and show mega menu', function() {
+          // TODO: Implement test.
+        } );
+      } );
     } );
   }
 } );
