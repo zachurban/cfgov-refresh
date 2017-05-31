9afc0867415a4ebf53c12059a90eea5355b97655
Andy Chosak <chosak@users.noreply.github.com>
2017-05-31 10:36:42 -0400

    experimenting

--- run_travis.sh
+++ run_travis.sh
@@ -5,6 +5,8 @@ set -e
 
 echo "running $RUNTEST tests"
 if [ "$RUNTEST" == "frontend" ]; then
+    ./frontend.sh test
+
     gulp "test:unit"
     gulp "test:coveralls"
 elif [ "$RUNTEST" == "backend" ]; then
@@ -12,9 +14,12 @@ elif [ "$RUNTEST" == "backend" ]; then
     flake8
     coveralls
 elif [ "$RUNTEST" == "acceptance" ]; then
+    ./frontend.sh test
+
     export DISPLAY=:99.0
     sh -e /etc/init.d/xvfb start &
     sleep 3
     export HEADLESS_CHROME_BINARY=/usr/bin/google-chrome-beta
+
     gulp test:acceptance
 fi
