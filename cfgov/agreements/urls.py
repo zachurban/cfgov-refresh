ee3d0b2426c79e4142d0ef5bc690562da82bcacc
higs4281 <higs4281@gmail.com>
2017-12-26 16:48:28 -0500

    Extend agreement models for SalesForce data
    When the SalesForce "Collect" portal starts handling the submission of
    credit card programs and related agreement PDFs, the available metadata
    will expland and become more reliable.
    
    This request adds models for prepay plans and extends the current models
    for issuers and agreements and offers API endpoints for elasticsearch.

--- cfgov/agreements/urls.py
+++ cfgov/agreements/urls.py
@@ -1,11 +1,31 @@
 from django.conf.urls import url
 
-from agreements.views import index, issuer_search
+from agreements.views import (
+    agreement_autocomplete, agreement_search, CreditDataView,
+    index, legacy_issuer_search
+)
 
 
 urlpatterns = [
     url(r'^$', index, name='agreements_home'),
     url(r'^issuer/(?P<issuer_slug>.*)/$',
-        issuer_search,
-        name='issuer_search'),
+        legacy_issuer_search,
+        name='legacy_issuer_search'),
+    url(r'^api/(?P<model>credit-agreement)/search/?$',
+        agreement_search,
+        name='agreement-search'),
+    url(r'^api/(?P<model>prepay-agreement)/search/?$',
+        agreement_search,
+        name='prepay-agreement-search'),
+    url(r'^api/(?P<model>issuer)/search/?$',
+        agreement_search,
+        name='issuer-search'),
+    url(r'^api/(?P<model>credit-agreement)/autocomplete/?$',
+        agreement_autocomplete, name='credit-agreement-autocomplete'),
+    url(r'^api/(?P<model>prepay-agreement)/autocomplete/?$',
+        agreement_autocomplete, name='prepay-agreement-autocomplete'),
+    url(r'^api/(?P<model>issuer)/autocomplete/?$',
+        agreement_autocomplete, name='issuer-autocomplete'),
+    url(r'^api/(?P<model>.*)/(?P<pk>\d{1,12})/?$',
+        CreditDataView.as_view(), name='agreement-credit-data'),
 ]
--- cfgov/agreements/views.py
+++ cfgov/agreements/views.py
@@ -1,9 +1,27 @@
+from __future__ import unicode_literals
+
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from django.http import Http404
+from django.http import Http404, JsonResponse
 from django.shortcuts import render
+from haystack.inputs import Clean
+from haystack.query import SearchQuerySet
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from agreements import RESULTS_PER_PAGE
-from agreements.models import Agreement, Issuer
+from agreements.models import (
+    Agreement, CreditPlan, Issuer, PrepayAgreement, PrepayPlan
+)
+
+
+MODEL_MAP = {
+    'credit-agreement': Agreement,
+    'credit-plan': CreditPlan,
+    'issuer': Issuer,
+    'prepay-agreement': PrepayAgreement,
+    'prepay-plan': PrepayPlan,
+}
 
 
 def index(request):
@@ -13,7 +31,7 @@ def index(request):
     })
 
 
-def issuer_search(request, issuer_slug):
+def legacy_issuer_search(request, issuer_slug):
     issuers = Issuer.objects.filter(slug=issuer_slug)
 
     if not issuers.exists():
@@ -27,7 +45,7 @@ def issuer_search(request, issuer_slug):
         issuer = issuers.latest('pk')
 
     pager = Paginator(
-        agreements.order_by('file_name'),
+        agreements.order_by('-posted', 'description'),
         RESULTS_PER_PAGE
     )
 
@@ -42,3 +60,53 @@ def issuer_search(request, issuer_slug):
         'page': page,
         'issuer': issuer,
     })
+
+
+def agreement_search(request, model):
+    search_model = MODEL_MAP.get(model)
+    clean_query = Clean(request.GET.get('q', ''))
+    qstring = clean_query.query_string.strip()
+    if not qstring or not search_model:
+        raise Http404
+    sqs = SearchQuerySet().models(search_model)
+    search = sqs.filter(content=clean_query)
+
+    results = [{'agreement': result.autocomplete,
+                'pk': result.pk,
+                'uri': result.uri,
+                'issuer': "{}".format(result.issuer),
+                'issuer_slug': result.issuer.slug if result.issuer else None,
+                'posted': result.posted}
+               for result in search]
+    return JsonResponse(results, safe=False)
+
+
+def agreement_autocomplete(request, model):
+    search_model = MODEL_MAP.get(model)
+    term = request.GET.get(
+        'term', '').strip().replace('<', '')
+    if not term or not search_model:
+        return JsonResponse([], safe=False)
+    sqs = SearchQuerySet().models(search_model).autocomplete(autocomplete=term)
+    results = [{'name': result.autocomplete,
+                'pk': result.pk}
+               for result in search[:20]]
+
+    return JsonResponse(results, safe=False)
+
+
+class CreditDataView(APIView):
+    """
+    API view for delivering credit objects by primary key
+    """
+    renderer_classes = (JSONRenderer,)
+
+    def get(self, request, model, pk):
+        search_model = MODEL_MAP.get(model)
+        if not search_model or not pk:
+            raise Http404("Invalid model")
+        try:
+            result = search_model.objects.get(pk=pk)
+        except search_model.DoesNotExist:
+            raise Http404("No {} found.".format(model))
+        return Response(result.payload)
