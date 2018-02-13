25a296d261dce54d2ec29d50dae7ad8da36742d4
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2018-02-13 18:26:11 -0500

    Add clean task callbacks

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -2,17 +2,20 @@ const configClean = require( '../config' ).clean;
 const del = require( 'del' );
 const gulp = require( 'gulp' );
 
-gulp.task( 'clean:css', () => {
+gulp.task( 'clean:css', done => {
   // Clean CSS out of /cfgov/static_built/css/
   del( configClean.css + '/**/*' );
+  done();
 } );
 
-gulp.task( 'clean:js', () => {
+gulp.task( 'clean:js', done => {
   // Clean JavaScript out of /cfgov/static_built/js/
   del( configClean.js + '/**/*' );
+  done();
 } );
 
-gulp.task( 'clean', () => {
+gulp.task( 'clean', done => {
   // Clean everything out of /cfgov/static_built/
   del( configClean.dest + '/**/*' );
+  done();
 } );
