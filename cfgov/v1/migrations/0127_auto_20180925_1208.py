e43ec6168a6fef463a63dd1f1faca25178870760
Virginia Czosek <Virginia.Czosek@cfpb.gov>
2018-09-25 09:17:45 -0700

    Recreate Ask block migrations.

--- cfgov/v1/migrations/0127_auto_20180925_1208.py
+++ cfgov/v1/migrations/0127_auto_20180925_1208.py
@@ -13,7 +13,7 @@ import v1.atomic_elements.organisms
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('v1', '0124_auto_20180906_1356'),
+        ('v1', '0126_add_conf_reg_answer_id_field'),
     ]
 
     operations = [
