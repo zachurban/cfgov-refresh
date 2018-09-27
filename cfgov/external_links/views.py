a03716a43a00ecd2307d714193d90bc5dd136065
Richa Agarwal <richa.agarwal85@gmail.com>
2018-09-27 02:50:26 -0700

    Use get_page_models instead of manually specifying models

--- cfgov/external_links/views.py
+++ cfgov/external_links/views.py
@@ -1,34 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+from wagtail.wagtailcore.models import get_page_models
+
 from external_links.forms import ExternalLinksForm
 
-from ask_cfpb.models.pages import AnswerPage
-from v1.models.base import CFGOVPage
-from v1.models.blog_page import BlogPage, LegacyBlogPage
-from v1.models.browse_filterable_page import BrowseFilterablePage
-from v1.models.browse_page import BrowsePage
-from v1.models.home_page import HomePage
-from v1.models.landing_page import LandingPage
-from v1.models.learn_page import (
-    AbstractFilterPage, DocumentDetailPage, EventPage, LearnPage
-)
 from v1.models.snippets import Contact, Resource, ReusableText
-from v1.models.sublanding_filterable_page import SublandingFilterablePage
-from v1.models.sublanding_page import SublandingPage
 
 
 class SearchView(View):
     template_name = 'search.html'
-    # These are all v1 models that directly implement a visible part
-    # of the Wagtail page, specifically the content, sidefoot, or header
-    v1_page_models = [
-        CFGOVPage, HomePage, AbstractFilterPage, BlogPage,
-        BrowsePage, BrowseFilterablePage,
-        DocumentDetailPage, EventPage,
-        LegacyBlogPage, LearnPage, LandingPage,
-        SublandingPage, SublandingFilterablePage,
-    ]
 
     def get(self, request):
         return render(request, self.template_name, {
@@ -40,12 +21,9 @@ class SearchView(View):
         url = form.data['url']
         pages = []
 
-        for cls in self.v1_page_models:
+        for cls in get_page_models():
             pages += list(cls.objects.search(url))
 
-        pages += list(AnswerPage.objects.search(url, fields=[
-            'answer', 'snippet'
-        ]))
         pages = sorted(pages, key=lambda k: k.title)
 
         contacts = list(
