90d476938e024114e97661a97a5d17751123c84a
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2018-10-18 20:01:37 -0700

    Embed build-satellites directly

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,16 @@ if [ ! -f $ENVVAR ]; then
   cp "$ENVVAR"_SAMPLE $ENVVAR
 fi
 
-bash -c './build-satellites.sh'
+# Build satellites
+for d in /usr/src/app/develop-apps/*/ ; do
+    if [ -d "$d" ]; then
+      export PYTHONPATH=$d:$PYTHONPATH
+      if [ ! -d "$d/node_modules" ]; then
+        echo "Building frontend for: $d"
+        "$d/frontend.sh"
+      fi
+    fi
+done
 
 source $ENVVAR
 python cfgov/manage.py runserver 0.0.0.0:8000
